b8fe872c4ad10e9270828131ec801a443ee0fc21
kumarsmt06 <kumarsmt.iitk@gmail.com>
2024-11-14 00:14:41 +0530

    function overload

---
+++ functions/funcOverload.java
@@ -0,0 +1,33 @@
+package functions;
+
+public class funcOverload {
+    /*
+     * area method
+     * Area of a square having side[float]
+     */
+    public static void area(float side){
+        System.out.println("Area of a square: " + (side * side));
+    }
+    
+    /*
+     * area method
+     * Area of a circle having radius[double]
+     */
+    public static void area(double radius){
+        System.out.println("Area of a Circle: " + (4.141 * radius * radius));
+    }
+    
+    /*
+     * area method
+     * Area of a rectangle having length l [float] and breadth b [float]
+     */
+    public static void area(float l, float b){
+        System.out.println("Area of a rectangle: " + (l * b));
+    }
+
+    public static void main(String[] args) {
+        area(2,3);
+        area(3);
+        area(4.23);
+    }
+}
